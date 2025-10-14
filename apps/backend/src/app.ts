d88875e86e3e6a4e7609eb518df3c9f4110025c6
oiviana <vianalucas80@gmail.com>
2025-10-14 14:35:54 -0300

    feat: add error boundary

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,10 +2,26 @@ import { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";
 import { prismaPlugin } from "./plugins/prisma";
 import { productRoutes } from "./routes/products";
+import { Prisma } from "@prisma/client"; 
 
 export async function app(server: FastifyInstance) {
+  // Middlewares
   await server.register(cors, { origin: true });
   await server.register(prismaPlugin);
 
+  // Error handler global
+  server.setErrorHandler((error, request, reply) => {
+    server.log.error(error); // log completo no console / arquivo
+
+    // Tratamento de erro do Prisma
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return reply.status(400).send({ message: error.message });
+    }
+
+    // Outros erros
+    return reply.status(500).send({ message: 'Erro interno do servidor' });
+  });
+
+  // Rotas
   server.register(productRoutes, { prefix: "/products" });
 }
