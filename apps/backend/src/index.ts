9caab7d54ddaf9bbaac7e7b7ce2ea8ff6baf4abd
oiviana <vianalucas80@gmail.com>
2025-10-15 12:27:19 -0300

    fix: using await at app

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,17 +1,18 @@
 import Fastify from "fastify";
 import { app } from "./app";
 
-const server = Fastify({
-  logger: true,
-});
-
-app(server);
+const server = Fastify({ logger: true });
 
 const start = async () => {
   try {
+ 
+    await app(server); 
     await server.listen({ port: 3333, host: "0.0.0.0" });
-    console.log("🚀 Server running on http://localhost:3333");
+
+    console.info("🚀 Server running on http://localhost:3333");
+
   } catch (err) {
+    console.error("erro: ",err);
     server.log.error(err);
     process.exit(1);
   }
