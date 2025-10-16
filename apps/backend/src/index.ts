d5c9eff9c1e0c5e50fb9a98fe81d0e74e80f4eb7
oiviana <vianalucas80@gmail.com>
2025-10-15 22:19:04 -0300

    refactor: fastfy server

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,18 +1,31 @@
 import Fastify from "fastify";
 import { app } from "./app";
 
-const server = Fastify({ logger: true });
+const serverPort = parseInt(process.env.SERVER_PORT || '3333', 10);
+
+const server = Fastify({
+  logger: {
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        ignore: 'pid',
+      }
+    }
+  }
+});
 
 const start = async () => {
   try {
- 
-    await app(server); 
-    await server.listen({ port: 3333, host: "0.0.0.0" });
 
-    console.info("Server running on http://localhost:3333");
+    await app(server);
+    await server.listen({ port: serverPort, host: "0.0.0.0" });
+
+    console.info(`index.ts: Server running at localhost on port ${serverPort}`);
 
   } catch (err) {
-    console.error("erro: ",err);
+    console.error("erro: ", err);
     server.log.error(err);
     process.exit(1);
   }
