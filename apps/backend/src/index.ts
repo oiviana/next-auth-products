5b926c4bcc1541942940bc0aa0733462778e5f2a
oiviana <vianalucas80@gmail.com>
2025-10-16 18:43:26 -0300

    feat: add jwt auth plugin

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,10 @@
-import Fastify from "fastify";
-import { app } from "./app";
+import Fastify from 'fastify';
+import { app } from './app';
+import authentication from '@plugins/authentication';
 
 const serverPort = parseInt(process.env.SERVER_PORT || '3333', 10);
 
-const server = Fastify({
+export const server = Fastify({
   logger: {
     level: 'info',
     transport: {
@@ -11,21 +12,19 @@ const server = Fastify({
       options: {
         colorize: true,
         ignore: 'pid',
-      }
-    }
-  }
+      },
+    },
+  },
 });
+
+server.register(authentication);
 
 const start = async () => {
   try {
-
     await app(server);
     await server.listen({ port: serverPort, host: "0.0.0.0" });
-
     console.info(`index.ts: Server running at localhost on port ${serverPort}`);
-
   } catch (err) {
-    console.error("erro: ", err);
     server.log.error(err);
     process.exit(1);
   }
