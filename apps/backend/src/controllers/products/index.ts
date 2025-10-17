63ee5438f2cf13a8ba00eeb602fa2ca40f450ebd
oiviana <vianalucas80@gmail.com>
2025-10-17 17:03:15 -0300

    feat: revenue, count products and sold products routes

--- apps/backend/src/controllers/products/index.ts
+++ apps/backend/src/controllers/products/index.ts
@@ -3,6 +3,7 @@ import { Product, Prisma } from "@prisma-generated/prisma";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { getUserIdByToken } from "@utils/getUserIdByToken";
 
+// Todos os produtos de um vendedor
 export async function getAllProductsBySeller(
   request: FastifyRequest,
   reply: FastifyReply
@@ -15,9 +16,6 @@ export async function getAllProductsBySeller(
           ownerId: userId,
         },
       },
-      include: {
-        store: true,
-      },
     });
 
     return reply.send(products);
@@ -27,7 +25,127 @@ export async function getAllProductsBySeller(
   }
 }
 
+// Conta todos os produtos de um vendedor
+export async function countAllProductsBySeller(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  try {
+    const userId = await getUserIdByToken(request);
 
+    const totalProducts = await prisma.product.count({
+      where: {
+        store: {
+          ownerId: userId,
+        },
+      },
+    });
+
+    return reply.send({ total: totalProducts });
+  } catch (error) {
+    request.server.log.error(error);
+    return reply.status(401).send({ message: "Não autorizado" });
+  }
+}
+
+// Conta todos os produtos vendidos
+export async function totalProductsSoldBySeller(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  try {
+    const userId = await getUserIdByToken(request);
+
+    // Soma de as colunas de quantidade
+    const result = await prisma.orderItem.aggregate({
+      _sum: {
+        quantity: true,
+      },
+      where: {
+        product: {
+          store: {
+            ownerId: userId,
+          },
+        },
+      },
+    });
+
+    const totalSold = result._sum.quantity ?? 0;
+
+    return reply.send({ totalSold });
+  } catch (error) {
+    request.server.log.error(error);
+    return reply.status(401).send({ message: "Não autorizado" });
+  }
+}
+
+// Total de faturamento de um vendedor
+export async function getTotalRevenueBySeller(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  try {
+    const userId = await getUserIdByToken(request);
+
+    // Buscar todos os pedidos que contêm produtos do vendedor
+    const orders = await prisma.order.findMany({
+      where: {
+        items: {
+          some: {
+            product: {
+              store: {
+                ownerId: userId,
+              },
+            },
+          },
+        },
+      },
+      select: {
+        total: true,
+      },
+    });
+
+    // Somar o campo total de cada pedido
+    const totalRevenue = orders.reduce((acc, order) => acc + order.total, 0);
+
+    return reply.send({ totalRevenue });
+  } catch (error) {
+    request.server.log.error(error);
+    return reply.status(401).send({ message: "Não autorizado" });
+  }
+}
+
+//Produto mais vendido de um vendedor
+export async function getMoreSoldProduct(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  try {
+    const userId = await getUserIdByToken(request);
+
+    const mostSoldProduct = await prisma.product.findFirst({
+      where: {
+        store: {
+          ownerId: userId,
+        },
+      },
+      orderBy: {
+        soldCount: 'desc',
+      },
+    });
+
+    if (!mostSoldProduct) {
+      return reply.status(404).send({ message: "Nenhum produto encontrado" });
+    }
+
+    return reply.send(mostSoldProduct);
+  } catch (error) {
+    request.server.log.error(error);
+    return reply.status(401).send({ message: "Não autorizado" });
+  }
+}
+
+// Cria um produto
 export async function createProduct(
   data: Prisma.ProductCreateInput
 ): Promise<Product> {
--- apps/backend/src/routes/products/index.ts
+++ apps/backend/src/routes/products/index.ts
@@ -1,24 +1,74 @@
 import { FastifyInstance } from "fastify";
-import { getAllProductsBySeller, createProduct } from "@controllers/products";
+import { getAllProductsBySeller, createProduct, getMoreSoldProduct, countAllProductsBySeller, totalProductsSoldBySeller, getTotalRevenueBySeller } from "@controllers/products";
 import { Prisma } from "@prisma-generated/prisma";
 
 export async function productRoutes(server: FastifyInstance) {
 
-  server.get("/", 
-     { preValidation: [server.authenticate] },
-     async (req, reply) => {
-    try {
-      const products = await getAllProductsBySeller(req, reply);
-      return reply.send(products);
-    } catch (error) {
-      server.log.error({ error }, "Erro ao buscar produtos");
-      return reply.status(500).send({ error: "Erro interno do servidor" });
-    }
-  });
+  server.get("/",
+    { preValidation: [server.authenticate] },
+    async (req, reply) => {
+      try {
+        const products = await getAllProductsBySeller(req, reply);
+        return reply.send(products);
+      } catch (error) {
+        server.log.error({ error }, "Erro ao buscar produtos");
+        return reply.status(500).send({ error: "Erro interno do servidor" });
+      }
+    });
+
+
+  server.get("/more-sold",
+    { preValidation: [server.authenticate] },
+    async (req, reply) => {
+      try {
+        const moreSoldProduct = await getMoreSoldProduct(req, reply);
+        return reply.send(moreSoldProduct);
+      } catch (error) {
+        server.log.error({ error }, "Erro ao buscar produto");
+        return reply.status(500).send({ error: "Erro interno do servidor" });
+      }
+    });
+
+  server.get("/all-products-by-seller",
+    { preValidation: [server.authenticate] },
+    async (req, reply) => {
+      try {
+        const allProductsBySeller = await countAllProductsBySeller(req, reply);
+        return reply.send(allProductsBySeller);
+      } catch (error) {
+        server.log.error({ error }, "Erro ao buscar produtos");
+        return reply.status(500).send({ error: "Erro interno do servidor" });
+      }
+    });
+
+  server.get("/all-products-sold-by-seller",
+    { preValidation: [server.authenticate] },
+    async (req, reply) => {
+      try {
+        const allProductsSold = await totalProductsSoldBySeller(req, reply);
+        return reply.send(allProductsSold);
+      } catch (error) {
+        server.log.error({ error }, "Erro ao buscar produtos");
+        return reply.status(500).send({ error: "Erro interno do servidor" });
+      }
+    });
+
+  server.get("/total-revenue-by-seller",
+    { preValidation: [server.authenticate] },
+    async (req, reply) => {
+      try {
+        const totalRevenue = await getTotalRevenueBySeller(req, reply);
+        return reply.send(totalRevenue);
+      } catch (error) {
+        server.log.error({ error }, "Erro ao buscar faturamento");
+        return reply.status(500).send({ error: "Erro interno do servidor" });
+      }
+    });
+
 
   server.post<{ Body: Prisma.ProductCreateInput }>(
     "/",
-    { preValidation: [server.authenticate] }, // fastfy auth
+    { preValidation: [server.authenticate] },
     async (req, reply) => {
       try {
         const data = req.body;
