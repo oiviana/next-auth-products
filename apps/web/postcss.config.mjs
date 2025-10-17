5d773b3efb551a667ca9e563c576d9f5283d05df
oiviana <vianalucas80@gmail.com>
2025-10-17 17:04:06 -0300

    chore: testing tailwind

--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,5 +1,12 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    '@tailwindcss/postcss': {
+      content: [
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}', // ✅ Adicione aqui também
+      ],
+    },
+  },
 };
 
 export default config;
