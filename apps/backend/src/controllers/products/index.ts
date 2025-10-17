c570a5b0def23e5ec585ed54efd77d38fd331f02
oiviana <vianalucas80@gmail.com>
2025-10-17 13:29:29 -0300

    feat: get products by user token

--- apps/backend/src/controllers/products/index.ts
+++ apps/backend/src/controllers/products/index.ts
@@ -1,9 +1,32 @@
 import prisma from "@lib/prisma";
-import { Prisma, Product } from "@prisma/client";
+import { Product, Prisma } from "@prisma-generated/prisma";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { getUserIdByToken } from "@utils/getUserIdByToken";
 
-export async function getAllProducts(): Promise<Product[]> {
-  return await prisma.product.findMany();
+export async function getAllProductsBySeller(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  try {
+    const userId = await getUserIdByToken(request);
+    const products = await prisma.product.findMany({
+      where: {
+        store: {
+          ownerId: userId,
+        },
+      },
+      include: {
+        store: true,
+      },
+    });
+
+    return reply.send(products);
+  } catch (error) {
+    request.server.log.error(error);
+    return reply.status(401).send({ message: "Não autorizado" });
+  }
 }
+
 
 export async function createProduct(
   data: Prisma.ProductCreateInput
