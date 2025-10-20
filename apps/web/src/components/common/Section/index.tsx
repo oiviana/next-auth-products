3c5e9e34ba72fc9ecd9b633331dd11022911b3e1
oiviana <vianalucas80@gmail.com>
2025-10-20 18:28:29 -0300

    style: adjust section padding

--- apps/web/src/components/common/Section/index.tsx
+++ apps/web/src/components/common/Section/index.tsx
@@ -6,7 +6,7 @@ interface SectionProps {
 
 export default function Section({ children }: SectionProps) {
     return (
-        <section className='min-h-30 pt-16 px-3 lg:pl-[400px]'>
+        <section className='min-h-30 pt-16 px-3 lg:pl-[300px]'>
             {children}
         </section>
     );
