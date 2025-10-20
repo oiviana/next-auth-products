be9bf1d8dd99b996dd013d4d38e27f209c02c6d6
oiviana <vianalucas80@gmail.com>
2025-10-20 13:29:32 -0300

    feat: signup link

--- apps/web/src/app/auth/page.tsx
+++ apps/web/src/app/auth/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import LoginForm from "@/components/forms/LoginForm";
 import LoadingScreen from "@/components/common/LoadingScreen";
+import Link from "next/link";
 
 export default function AuthPage() {
   const { user, loading } = useAuth();
@@ -18,7 +19,7 @@ export default function AuthPage() {
       if (user.role === "SELLER") {
         router.replace("/seller");
       } else if (user.role === "CLIENT") {
-        router.replace("/client");
+        router.replace("/user");
       }
     }
   }, [loading, user, router, redirecting]);
@@ -26,8 +27,21 @@ export default function AuthPage() {
   if (loading || redirecting) return <LoadingScreen />;
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <LoginForm />
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="max-w-md w-full space-y-8 p-8">
+        <LoginForm />
+        <div className="text-center">
+          <p className="text-gray-600">
+            Não possui uma conta?{" "}
+            <Link 
+              href="auth/signup" 
+              className="text-blue-600 hover:text-blue-500 font-medium transition-colors"
+            >
+              Cadastre-se aqui!
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
